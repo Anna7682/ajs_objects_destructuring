718dbcfefeb94937accae8d1114d1e8cb7bf36a5
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-02-02 11:49:50 +0300

    Add files via upload

--- src/getUserCharacteristic.js
+++ src/getUserCharacteristic.js
@@ -1,1 +1,37 @@
+// export default function getUserСharacteristic(objProperty) {
+// 	const resultArr = [];
 
+// 	objProperty.forEach((item) => {
+// 		item.description = item.description ? item.description : 'Описание недоступно';
+
+// 		resultArr.push(item);
+// 	});
+
+// 	return resultArr;
+// }
+
+// export default function getUserСharacteristic(object) {
+//   const resultArr = [];
+
+//   object.special.forEach((item) => {
+//     const {
+//  id, name, icon, description = 'Описание недоступно',
+// } = item;
+//     // console.log(id, name, icon, description);
+
+//     const obj = {};
+//     obj.id = id;
+//     obj.name = name;
+//     obj.icon = icon;
+//     obj.description = description;
+//     // console.log(obj);
+
+//     resultArr.push(obj);
+//   });
+
+//   return resultArr;
+// }
+
+export default function getUserСharacteristic({ special = [] }) {
+  return special.map(({ description = 'Описание недоступно', ...item }) => ({ ...item, description }));
+}
